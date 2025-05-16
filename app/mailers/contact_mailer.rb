cc50723a0bacd583169eb2e27fd3697b2652e979
arnaudlothe <arnaud.lothe@gmail.com>
2025-05-16 09:14:12 +0200

    changement de mail contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 # app/mailers/contact_mailer.rb
 class ContactMailer < ApplicationMailer
-  default to: "mickael@tetris-vv.fr" # Destinataire de l'email
+  default to: "contact@tetris-vv.fr" # Destinataire de l'email
 
   def contact_email(contact)
     @contact = contact
